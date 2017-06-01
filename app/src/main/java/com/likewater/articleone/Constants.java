218560acffee4460eaa555f5dabd21922c704319
Blake <lakebayomackway@gmail.com>
2017-06-01 11:05:05 -0700

    add api key,Okhttp dependency, Constants.java

---
+++ app/src/main/java/com/likewater/articleone/Constants.java
@@ -0,0 +1,10 @@
+package com.likewater.articleone;
+
+public class Constants {
+    public static final String PRO_PUBLICA_KEY = BuildConfig.PRO_PUBLICA_KEY;
+
+//    public static final String API_KEY = BuildConfig.API_KEY;
+//    public static final String API_BASE_URL = "www.<api url>.com";
+//    public static final String YOUR_QUERY_PARAMETER = "<your query parameter string>"; //Example: "location"
+//    public static final String API_KEY_QUERY_PARAMETER = "appid";
+}
